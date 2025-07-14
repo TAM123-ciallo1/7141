3a3b74436af4c9e328b3801bb77068616f619905
TAM123-ciallo1 <2296674725@qq.com>
2025-07-14 19:31:06 +0800

    Delete spring-p directory

--- spring-p/src/main/java/com/itheima/springp/repository/DeviceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.itheima.springp.repository;
-
-import com.itheima.springp.model.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, String> {}
--- spring-p/src/main/java/com/itheima/springp/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.itheima.springp.repository;
-
-import com.itheima.springp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {}
